16c634069d4efd8c09790a8aa695490b79698262
Satyam <satyamsurade06@gmail.com>
2025-08-20 20:00:35 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 const App = () => {
     return (
         <div className="overflow-x-hidden text-neutral-300 antialiased
